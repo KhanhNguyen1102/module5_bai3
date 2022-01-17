5a8ac5994fbf43b0873c5c102c860fd8863c4c60
KhanhNguyen1102 <sukhanh12@gmail.com>
2022-01-17 16:39:51 +0700

    update_form_product

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from "../model/product";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-product',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  listProduct: Product[] = [
+    {
+      id:'1',
+      name:'IP12',
+      price:'1200'
+    },
+    {
+      id:'2',
+      name:'IP13',
+      price:'1400'
+    },
+    {
+      id:'3',
+      name:'IP14',
+      price:'1500'
+    }
+  ]
+  productForm = new FormGroup({
+    id: new FormControl(''),
+    name: new FormControl(''),
+    price: new FormControl('')
+  });
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addProduct() {
+    this.listProduct.push(this.productForm.value)
+    console.log(this.productForm.value)
+  }
 }
